04ba9c1c6734113e383a6e98c2e8c9e40bd35b49
Alex Lewis <alexlewis374@gmail.com>
2020-05-08 16:39:09 +0100

    test - returns the word passes

--- spec/letter_spec.rb
+++ spec/letter_spec.rb
@@ -4,5 +4,8 @@ describe Letter do
     it 'it returns the letter' do
         expect(subject).to respond_to(:get_middle).with(1).argument
     end
-    
+
+    it "returns the word" do
+        expect(subject.get_middle("A")).to eq('A')
+    end 
 end
